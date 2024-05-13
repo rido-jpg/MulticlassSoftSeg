1b3f0bcba93acf090cb1c00c9459b982d334556f
rido-jpg <razai.farid@gmail.com>
2024-05-13 16:09:02 +0200

    Added global array of contrasts.
    Modified parameters of BidsDataset Class and modified the init and get_item accordingly (dataset now saves the paths and only loads files when getitem is called).
    
    Modified create_bids_path_list_of_dicts, to work with .fnio files.
    
    Modified load_nifti_as_array() to return contiguouos arrays to work with fast numpy io.
    
    Added functions to split the data (train/val) and save the split in a csv.
    
    Added functions to convert nifti files to fast numpy io (fnio) format and then save them.

--- src/data/bids_dataset.py
+++ src/data/bids_dataset.py
@@ -3,17 +3,34 @@ import nibabel as nib
 import numpy as np
 import torch
 import torch.nn.functional as F
+import random
+import csv
+import sys
+import TPTBox
+
 from torchvision import transforms
 from torch.utils.data import Dataset
 from pathlib import Path
 
+file = Path(__file__).resolve()
+sys.path.append(str(file.parents[1]))
+sys.path.append(str(file.parents[2]))
+
+# os.chdir(os.path.expanduser('~/farid_ma/dev/multiclass_softseg/MulticlassSoftSeg'))
+from external.fastnumpyio.fastnumpyio import save, load, pack, unpack
+
+# Array of all MRI contrasts and the segmentation mask  
+contrasts = ['t1c', 't1n', 't2f', 't2w', 'seg']
+
 class BidsDataset(Dataset):
-    def __init__(self, root_dir, prefix="", contrast='t2f', binary=True, transform=None, padding=(256, 256)):
+    def __init__(self, root_dir:str, prefix:str="", contrast:str='t2f', suffix:str='fnio', do2D:bool=True, binary:bool=True, transform=None, padding=(256, 256)):
         """
         Parameters:
         root_dir (str): The root directory containing subdirectories for each subject.
         prefix (str): if all folders have the same prefix e.g. "BraTS-GLI-" you can add it to be excluded from the dict key
         contrast (str): type of mri contrast you want to use for the dataset, options: 't1c', 't1n', 't2f', 't2w'
+        suffix (str): The file extension of the MRI and segmentation files. Can be modified if we saved the files in different format than .nii.gz, e.g. in fast numpy io format "fnio"
+        do2D (bool): if True, use 2D slices of the MRI images and segmentation masks
         binary (bool): if True, convert segmentation mask to binary classification labels (tumor vs. non-tumor)
         transform(): transformation of image data
         """
@@ -21,28 +38,48 @@ class BidsDataset(Dataset):
         self.root_dir = root_dir   
         self.prefix = prefix     
         self.contrast = contrast
+        self.suffix = suffix
+        self.do2D = do2D
         self.transform = transform
         self.binary = binary
         self.padding = padding
 
-        self.bids_list = create_bids_array_list_of_dicts(self.root_dir, prefix=self.prefix)
+        self.bids_list = create_bids_path_list_of_dicts(self.root_dir, prefix=self.prefix, suffix=self.suffix)
         
     def __len__(self):
         return len(self.bids_list)
     
     def __getitem__(self, idx):
-        img = self.bids_list[idx][self.contrast]
-        seg = self.bids_list[idx]['seg']
+        img_path = self.bids_list[idx][self.contrast]
+        mask_path = self.bids_list[idx]['seg']
 
-        seg = np.array(seg, dtype=np.int64)   # Ensure mask is integer type
+        # Load the MRI image and segmentation mask
+        if self.suffix == 'fnio':
+            img = load(str(img_path))
+            mask = load(str(mask_path))
+
+        elif self.suffix == 'nii.gz':
+            img = load_nifti_as_array(str(img_path))
+            mask = load_nifti_as_array(str(mask_path))
+
+        mask = np.array(mask, dtype=np.int64)   # Ensure mask is integer type
+
+        # Normalize the MRI image
+        img = normalize(img)  
+        
+        # Get a 2D slice if specified
+        if self.do2D:
+            #img = img.squeeze(0)
+            #mask = mask.squeeze(0)
+            img, mask = get_central_slice(img), get_central_slice (mask)      
 
         # Convert segmentation mask to binary classification labels (tumor vs. non-tumor)
         if self.binary:
-            seg[seg > 0] = 1
+            mask[mask > 0] = 1
 
         # Convert numpy arrays to PyTorch tensors and add a channel dimension
         img = torch.from_numpy(img).unsqueeze(0).float()
-        seg = torch.from_numpy(seg).unsqueeze(0).long()
+        mask = torch.from_numpy(mask).unsqueeze(0).long()
 
         if self.padding:
             # Calculate the padding required to achieve the desired size
@@ -53,14 +90,14 @@ class BidsDataset(Dataset):
             
             # Apply padding to the image and segmentation mask
             img = F.pad(img, (0, pad_w, 0, pad_h))
-            seg = F.pad(seg, (0, pad_w, 0, pad_h))
+            mask = F.pad(mask, (0, pad_w, 0, pad_h))
         
         if self.transform:
-            img, seg = self.transform(img, seg)
+            img, mask = self.transform(img, mask)
         
-        return img, seg
+        return img, mask
 
-def create_bids_path_list_of_dicts(root_dir, prefix=""):
+def create_bids_path_list_of_dicts(root_dir:str, prefix:str="", suffix:str="nii.gz")->list:
     """
     Scan the specified directory for MRI and segmentation files organized in a BIDS-like structure.
 
@@ -69,28 +106,29 @@ def create_bids_path_list_of_dicts(root_dir, prefix=""):
 
     prefix (str): if all folders have the same prefix e.g. "BraTS-GLI-" you can add it to be excluded from the dict key
 
+    suffix (str): The file extension of the MRI and segmentation files. Can be modified if we saved the files in different format than .nii.gz, e.g. in fast numpy io format "fnio"
+
     Returns:
     dict: A dictionary where each key is a subject directory name, and the value is another
           dictionary with keys for each MRI type and the segmentation mask, containing their file paths.
     """
-    mri_types = ['t1c', 't1n', 't2f', 't2w', 'seg']
     list_of_path_dicts = []
     root_path = Path(root_dir)
 
     # Iterate through each directory in the root directory
     for folder_path in root_path.iterdir():
         if folder_path.is_dir():
-            #Initialize a sub-dictionary for each subject
+            # Initialize a sub-dictionary for each subject
             folder = folder_path.name
             modified_folder_name = folder.replace(prefix, "") if folder.startswith(prefix) else folder
-            subject_files = {mri_type: None for mri_type in mri_types}
+            subject_files = {contrast: None for contrast in contrasts}
 
             # Search for MRI and segmentation files
             for file_path in folder_path.iterdir():
                 file_name = file_path.name
-                for mri_type in mri_types:
-                    if f"{folder}-{mri_type}.nii.gz" in file_name:
-                        subject_files[mri_type] = file_path.absolute()
+                for contrast in contrasts:
+                    if f"{folder}-{contrast}.{suffix}" in file_name:
+                        subject_files[contrast] = file_path.absolute()
 
             # Add the dictionary for the current subject to the main list
             subject_dict = {'subject': modified_folder_name, **subject_files}
@@ -98,14 +136,14 @@ def create_bids_path_list_of_dicts(root_dir, prefix=""):
 
     return list_of_path_dicts
 
-def create_bids_array_list_of_dicts(root_dir, prefix=""):
+def create_bids_array_list_of_dicts(root_dir:str, prefix:str= "") -> list:
     list_of_path_dicts = create_bids_path_list_of_dicts(root_dir, prefix)
     list_of_array_dicts = []
 
     for subject_dict in list_of_path_dicts:
         subject = subject_dict['subject']
         array_dict = {'subject': subject}
-        for contrast in ['t1c', 't1n', 't2f', 't2w', 'seg']: # List MRI types
+        for contrast in contrasts:  # MRI types from the contrasts list
             filepath = subject_dict.get(contrast)
             if filepath and Path(filepath).exists():  # Ensure the file exists
                 if contrast == 'seg':
@@ -120,13 +158,13 @@ def create_bids_array_list_of_dicts(root_dir, prefix=""):
 
     return list_of_array_dicts
 
-def load_nifti_as_array(file_path):
+def load_nifti_as_array(file_path: str):
     #Load nifti file and convert to np array
     nifti_image = nib.load(file_path)
     nifti_np_array = np.array(nifti_image.get_fdata(), dtype=np.float64)
-    return nifti_np_array
+    return np.ascontiguousarray(nifti_np_array) # Ensure C-contiguity for fast numpy io
 
-def get_central_slice(nifti_np_array, axis=2):
+def get_central_slice(nifti_np_array: np.ndarray, axis:int=2)->np.ndarray:
     """
     Export the central slice of a NIfTI image along the specified axis.
     
@@ -152,7 +190,7 @@ def get_central_slice(nifti_np_array, axis=2):
 
     return slice_data
 
-def normalize(nifti_array):
+def normalize(nifti_array:np.ndarray) -> np.ndarray:
     # Z-Score standardization
     # Mask to exclude zeros
     mask = nifti_array != 0
@@ -173,8 +211,57 @@ def normalize(nifti_array):
     
     return normalized_array
 
-def load_normalized_central_slice_as_array(file_path):
+def load_normalized_central_slice_as_array(file_path: str)->np.ndarray:
     nifti_array = load_nifti_as_array(file_path)
     normalized_array = normalize(nifti_array)
     central_slice = get_central_slice(normalized_array)
     return central_slice   
+
+def split_data_and_save_to_csv(data: list, split_percentage: float, directory: str, mode: str = "train_val", seed: int = 42): # necessary?
+    # Set the seed for reproducibility
+    random.seed(seed)
+
+    # Shuffle the data
+    random.shuffle(data)
+
+    # Calculate the index at which to split the data
+    split_index = int(len(data) * split_percentage)
+
+    
+
+    # Split the data into training and validation sets
+    train_data = [{'split': 'train', 'subject': d.get('subject', '')} for d in data[:split_index]]
+    val_data = [{'split': 'val', 'subject': d.get('subject', '')} for d in data[split_index:]]
+
+    # Create the directory if it doesn't exist
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # Save the "subject" values from the training and validation sets to CSV files
+    _save_to_csv(train_data + val_data, Path(directory) / 'train_val_split.csv')
+
+def _save_to_csv(data: list, filename: Path):
+    with filename.open('w', newline='') as csvfile:
+        fieldnames = ['split', 'subject']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for d in data:
+            writer.writerow(d)
+
+def convert_nifti_to_fnio(file: str):
+    # Save the NIfTI file as a fnio array in the same directory, with the same name
+    nifti_np_array = load_nifti_as_array(file)
+    new_file_name = file.replace('.nii.gz', '.fnio')
+    save(new_file_name, nifti_np_array)
+
+def save_bids_niftis_as_fnio(list_of_path_dicts:list):
+    for subject_dict in list_of_path_dicts:
+        for contrast in contrasts:
+            filepath = subject_dict.get(contrast)
+            print(f"subject: {subject_dict.get('subject')} and contrast: {contrast}")
+            #print(filepath)
+            if filepath and Path(filepath).exists():  # Ensure the file exists
+                path_as_string = str(filepath)
+                convert_nifti_to_fnio(path_as_string)
+        print(f"subject {subject_dict.get('subject')} done.")
+        print(f"-------------------------------")
