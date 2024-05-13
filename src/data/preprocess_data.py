d61402915c67f43cee5217545d3e00ea85869d37
rido-jpg <razai.farid@gmail.com>
2024-05-13 15:58:33 +0200

    script that loops over BIDS folder structure and saves all files in fnio format (only needs to be used once)

---
+++ src/data/preprocess_data.py
@@ -0,0 +1,10 @@
+from bids_dataset import create_bids_path_list_of_dicts, save_bids_niftis_as_fnio
+from pathlib import Path
+
+# Preprocess Nifti images used for training, validation and testing
+# A script that is only used once, which loops over the whole BIDS folder structure, and saves all files in fnio (fast numpy io) format in the same folders with the same name (just the file ending is replaced by .fnio).
+
+bids_root_path = '/media/DATA/farid_ma/dev/multiclass_softseg/MulticlassSoftSeg/data/external/ASNR-MICCAI-BraTS2023-GLI-Challenge/ASNR-MICCAI-BraTS2023-GLI-Challenge-TrainingData'
+list_of_path_dicts = create_bids_path_list_of_dicts(bids_root_path)
+
+save_bids_niftis_as_fnio(list_of_path_dicts)
