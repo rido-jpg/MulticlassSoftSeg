31b62931d08bc85ba37a3e027dd4e3720ec1bbc2
rido-jpg <razai.farid@gmail.com>
2024-05-30 15:50:53 +0200

    Changed __getitem__() of bids_dataset to return dict that's compatible with MONAI transforms
    Changed setup() of BidsDataModule to create train_dataset/test_dataset/val_dataset using the files from the respective folders that are prepared by the new mv_files_to_folders().
    
    Changed def split_data_and_save_to_csv() to create train,val and test split at once and save it to a csv.
    
    Wrote mv_files_to_folders() which then moves them to train, val and test subdirectories.
    
    Outsourced the preprocessing steps from __getitem__() to preprocess()

--- src/data/bids_dataset.py
+++ src/data/bids_dataset.py
@@ -1,8 +1,11 @@
 import os
+import logging
 #import nibabel as nib
 import random
 import csv
 import sys
+import math
+from lightning.pytorch.utilities.types import EVAL_DATALOADERS
 import torch
 import torch.nn.functional as F
 import numpy as np
@@ -10,48 +13,72 @@ import lightning.pytorch as pl
 
 
 from TPTBox import NII
+from torch import nn
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader, random_split
 from pathlib import Path
+
+from monai.transforms import (
+    Activations,
+    Compose,
+    EnsureChannelFirstd,
+    LoadImaged,
+    RotateD, 
+    RandRotateD,
+    ScaleIntensityd
+)
 
 file = Path(__file__).resolve()
 sys.path.append(str(file.parents[1]))
 sys.path.append(str(file.parents[2]))
 
-# os.chdir(os.path.expanduser('~/farid_ma/dev/multiclass_softseg/MulticlassSoftSeg'))
-from external.fastnumpyio.fastnumpyio import save, load, pack, unpack
+import external.fastnumpyio.fastnumpyio as fnio
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 # Array of all MRI contrasts and the segmentation mask  
 contrasts = ['t1c', 't1n', 't2f', 't2w', 'seg']
 
+# Keys of Dictionary that will be returned by the Dataset
+brats_keys = ['img', 'seg']
+
+# # Train Transforms
+# train_transforms = Compose(
+#     [
+#         RandRotateD(keys=brats_keys, range_x=[- math.radians(30), math.radians(30)], prob=1, mode =["bilinear", "nearest"]),
+#     ]
+# )
+
 class BidsDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir:str, contrast:str='t2f', format:str='fnio', do2D:bool=True, binary:bool=True, transform=None, padding=(256, 256), batch_size:int=2):
+    def __init__(self, data_dir:str, contrast:str='t2f', format:str='fnio', do2D:bool=True, binary:bool=True, train_transform=None, test_transform=None, padding=(256, 256), batch_size:int=2):
         super().__init__()
         self.data_dir = data_dir
         self.contrast = contrast
         self.format = format
         self.do2D = do2D
         self.binary = binary
-        self.transform = transform
+        self.test_transform = test_transform
         self.padding = padding
-        self.batch_size = batch_size
+        self.batch_size = batch_size 
+        self.train_transform = train_transform   
 
     def setup(self, stage: str = None) -> None:
-        # Assign train/val datasets for use in dataloaders
-        full_dataset = BidsDataset(self.data_dir, contrast=self.contrast, suffix=self.format, do2D=self.do2D, binary=self.binary, transform=self.transform,padding=self.padding)
-        train_size = int(0.8 * len(full_dataset))
-        val_size = len(full_dataset) - train_size
-        generator = torch.Generator().manual_seed(42)   # Set seed for reproducibility
-        self.train_dataset, self.val_dataset = random_split(full_dataset, [train_size, val_size], generator)
-
+        self.train_dataset = BidsDataset(self.data_dir +'/train', contrast=self.contrast, suffix=self.format, do2D=self.do2D, binary=self.binary, transform=self.train_transform,padding=self.padding)
+        self.val_dataset = BidsDataset(self.data_dir + '/val', contrast=self.contrast, suffix=self.format, do2D=self.do2D, binary=self.binary, transform=self.test_transform,padding=self.padding)
+        self.test_dataset = BidsDataset(self.data_dir + '/test', contrast=self.contrast, suffix=self.format, do2D=self.do2D, binary=self.binary, transform=self.test_transform,padding=self.padding)
+        
     def train_dataloader(self) -> torch.Any:
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, drop_last=True, num_workers=19)
     
     def val_dataloader(self) -> torch.Any:
         return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=19)
 
+    def test_dataloader(self) -> torch.Any:
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=19)
+
 class BidsDataset(Dataset):
-    def __init__(self, root_dir:str, prefix:str="", contrast:str='t2f', suffix:str='fnio', do2D:bool=True, binary:bool=True, transform=None, padding=(256, 256)):
+    def __init__(self, root_dir:str, prefix:str="", contrast:str='t2f', suffix:str='fnio', do2D:bool=True, binary:bool=True, transform=None, padding:tuple[float,float]=(256, 256)):
         """
         Parameters:
         root_dir (str): The root directory containing subdirectories for each subject.
@@ -68,9 +95,13 @@ class BidsDataset(Dataset):
         self.contrast = contrast
         self.suffix = suffix
         self.do2D = do2D
+        #self.preprocess = Preprocess()
         self.transform = transform
         self.binary = binary
         self.padding = padding
+        self.dict_keys = ['img', 'seg']
+        self.img_key = self.dict_keys[0]
+        self.seg_key = self.dict_keys[1]
 
         self.bids_list = create_bids_path_list_of_dicts(self.root_dir, prefix=self.prefix, suffix=self.suffix)
         
@@ -78,13 +109,16 @@ class BidsDataset(Dataset):
         return len(self.bids_list)
     
     def __getitem__(self, idx):
+        """"
+        returns: dict with keys 'img' and 'seg' containing the MRI image and segmentation mask
+        """
         img_path = self.bids_list[idx][self.contrast]
-        mask_path = self.bids_list[idx]['seg']
+        mask_path = self.bids_list[idx][self.seg_key]
 
         # Load the MRI image and segmentation mask
         if self.suffix == 'fnio':
-            img = load(str(img_path))
-            mask = load(str(mask_path))
+            img = fnio.load(str(img_path))
+            mask = fnio.load(str(mask_path))
 
         elif self.suffix == 'nii.gz':
             img = load_nifti_as_array(str(img_path))
@@ -92,38 +126,28 @@ class BidsDataset(Dataset):
 
         mask = np.array(mask, dtype=np.int64)   # Ensure mask is integer type
 
-        # Normalize the MRI image
-        img = normalize(img)  
-        
-        # Get a 2D slice if specified
-        if self.do2D:
-            #img = img.squeeze(0)
-            #mask = mask.squeeze(0)
-            img, mask = get_central_slice(img), get_central_slice (mask)      
+        img = preprocess(img, False, self.do2D, self.binary, self.padding)
+        mask = preprocess(mask, True, self.do2D, self.binary, self.padding)
 
-        # Convert segmentation mask to binary classification labels (tumor vs. non-tumor)
-        if self.binary:
-            mask[mask > 0] = 1
-
-        # Convert numpy arrays to PyTorch tensors and add a channel dimension
-        img = torch.from_numpy(img).unsqueeze(0).float()
-        mask = torch.from_numpy(mask).unsqueeze(0).long()
-
-        if self.padding:
-            # Calculate the padding required to achieve the desired size
-            h, w = img.shape[-2:]
-            target_h, target_w = self.padding
-            pad_h = max(0, target_h - h)
-            pad_w = max(0, target_w - w)
+        # if self.padding:    # Move to transform or _preprocess?
+        #     # Calculate the padding required to achieve the desired size
+        #     h, w = img.shape[-2:]
+        #     target_h, target_w = self.padding
+        #     pad_h = max(0, target_h - h)
+        #     pad_w = max(0, target_w - w)
             
-            # Apply padding to the image and segmentation mask
-            img = F.pad(img, (0, pad_w, 0, pad_h))
-            mask = F.pad(mask, (0, pad_w, 0, pad_h))
-        
+        #     # Apply padding to the image and segmentation mask
+        #     img = F.pad(img, (0, pad_w, 0, pad_h))
+        #     mask = F.pad(mask, (0, pad_w, 0, pad_h))
+
+        data_dict = {self.img_key: img, self.seg_key: mask}
+
         if self.transform:
-            img, mask = self.transform(img, mask)
+            data_dict = self.transform(data_dict)
+
+        logging.info(f"Loaded MRI image and segmentation mask for subject {self.bids_list[idx]['subject']}.")
         
-        return img, mask
+        return data_dict
 
 def create_bids_path_list_of_dicts(root_dir:str, prefix:str="", suffix:str="nii.gz")->list:
     """
@@ -245,51 +269,42 @@ def load_normalized_central_slice_as_array(file_path: str)->np.ndarray:
     central_slice = get_central_slice(normalized_array)
     return central_slice   
 
-def split_data_and_save_to_csv(data: list, split_percentage: float, directory: str, train_val:bool = True, seed: int = 42):
+def split_data_and_save_to_csv(data: list, split_percentage: tuple[float, float, float], directory: str, seed: int = 42):
     """
-    Split the data into sets for either training and testing or training and validation and save the "subject" values to CSV files.
+    Split the data into sets for training, validation, and testing and save the "subject" values to CSV files.
     Parameters:
     data (list): The list of data dictionaries to be split.
-    split_percentage (float): The percentage of data to be used for training.
-    directory (str): The directory in which to save the CSV files.
-    train_val (bool): If True, split the data into training and validation sets. If False, split the data into training and testing sets.
+    split_percentage (tuple): The percentage of data to be used for training, validation, and testing. First element is for training, second for validation, and third for testing.
     seed (int): The seed for the random number generator.
     """
+    
+    # Check that the split percentages add up to 1
+    if sum(split_percentage) != 1:
+        raise ValueError("The sum of the split percentages must be equal to 1.")
+    
     # Set the seed for reproducibility
     random.seed(seed)
 
     # Shuffle the data
     random.shuffle(data)
+    
+    train_percentage = split_percentage[0]
+    val_percentage = split_percentage[1]
 
     # Calculate the index at which to split the data
-    split_index = int(len(data) * split_percentage)
+    train_test_split_index = int(len(data) * (train_percentage + val_percentage))
+    train_val_split_index = int(len(data) * train_percentage)
 
-    # Check if the data should be split into training and validation or training and testing sets
-    if train_val:
-        _split_and_save(True, data, split_index, directory)
+    # Split the data into training, validation, and testing sets
+    train_data = [{'split': 'train', 'subject': d.get('subject', '')} for d in data[:train_val_split_index]]
+    val_data = [{'split': 'val', 'subject': d.get('subject', '')} for d in data[train_val_split_index:train_test_split_index]]
+    test_data = [{'split': 'test', 'subject': d.get('subject', '')} for d in data[train_test_split_index:]]
 
-    else:
-        _split_and_save(False, data, split_index, directory)
+    # Create the directory if it doesn't exist
+    Path(directory).mkdir(parents=True, exist_ok=True)
 
-def _split_and_save(val_split: bool, data: list, split_index: int, directory: str):
-        """"
-        Parameters:
-        val_split (bool): If True, split the data into training and validation sets. If False, split the data into training and testing sets.
-        """
-        if val_split:
-            rest = 'val'
-        else:
-            rest = 'test'
-
-        # Split the data into training and validation/test sets
-        train_data = [{'split': 'train', 'subject': d.get('subject', '')} for d in data[:split_index]]
-        rest_data = [{'split': rest, 'subject': d.get('subject', '')} for d in data[split_index:]]
-
-        # Create the directory if it doesn't exist
-        Path(directory).mkdir(parents=True, exist_ok=True)
-
-        # Save the "subject" values from the training and validation/test sets to CSV files
-        _save_to_csv(train_data + rest_data, Path(directory) / f"train_{rest}_split.csv")
+    # Save the "subject" values from the training, validation, and testing sets to CSV files
+    _save_to_csv(train_data + val_data + test_data, Path(directory) / "train_val_test_split.csv")
 
 def _save_to_csv(data: list, filename: Path):
     with filename.open('w', newline='') as csvfile:
@@ -300,11 +315,41 @@ def _save_to_csv(data: list, filename: Path):
         for d in data:
             writer.writerow(d)
 
+def mv_files_to_folders(data: str, source_dir: str, target_dir: str):
+    """
+    Move files from a source directory to subdirectories in a target directory based on the subject names and the split.
+    Parameters:
+    data (str): Path to a CSV file containing the list of data dictionaries with the subject names.
+    source_dir (str): The directory containing the directory to be moved.
+    target_dir (str): The directory to which the directories will be moved.
+    """
+    # Load the data from the CSV file
+    with open(data, mode='r') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        data = list(csvreader)
+        
+    # Convert target_dir to a Path object
+    target_dir = Path(target_dir)
+
+    # Create the target directories if they don't exist
+    (target_dir / 'train').mkdir(parents=True, exist_ok=True)
+    (target_dir / 'val').mkdir(parents=True, exist_ok=True)
+    (target_dir / 'test').mkdir(parents=True, exist_ok=True)
+
+    # Move the folders for the train, val and test subjects to the 'train', 'val' and 'test' directories respectively
+    for folder in data:
+        subject = folder.get('subject', '')
+        split = folder.get('split', '')
+        source_folder = Path(source_dir) / subject
+        target_folder = Path(target_dir) / split / subject
+        # Move the folder to the target directory
+        source_folder.replace(target_folder)
+
 def convert_nifti_to_fnio(file: str):
     # Save the NIfTI file as a fnio array in the same directory, with the same name
     nifti_np_array = load_nifti_as_array(file)
     new_file_name = file.replace('.nii.gz', '.fnio')
-    save(new_file_name, nifti_np_array)
+    fnio.save(new_file_name, nifti_np_array)
 
 def save_bids_niftis_as_fnio(list_of_path_dicts:list):
     for subject_dict in list_of_path_dicts:
@@ -317,3 +362,70 @@ def save_bids_niftis_as_fnio(list_of_path_dicts:list):
                 convert_nifti_to_fnio(path_as_string)
         print(f"subject {subject_dict.get('subject')} done.")
         print(f"-------------------------------")
+
+def preprocess(img:np.array, seg:bool, do2D:bool, binary:bool, padding:tuple[int, int]) -> torch.Tensor:
+        #Normalize the MRI image
+        if not seg:
+            img = normalize(img)
+
+        # Get a 2D slice if specified
+        if do2D:
+            img= get_central_slice(img)
+
+        # Convert segmentation mask to binary classification labels (tumor vs. non-tumor)
+        if binary and seg:
+            img[img > 0] = 1
+
+        # Convert numpy arrays to PyTorch tensors
+        img = torch.from_numpy(img)
+
+        # Add a channel dimension if it's missing
+        if img.ndim == 2:  # if the image is 2D
+            img = img.unsqueeze(0)
+        elif img.ndim == 3 and seg:  # if the segmentation mask is 3D
+            img = img.unsqueeze(0)
+
+        # Ensure the image is in the correct type
+        if not seg:
+            img = img.float()
+        else:
+            img = img.long()
+
+        if padding:    # Move to transform?
+            # Calculate the padding required to achieve the desired size
+            h, w = img.shape[-2:]
+            target_h, target_w = padding
+            pad_h = max(0, target_h - h)
+            pad_w = max(0, target_w - w)
+            
+            # Apply padding to the image
+            img = F.pad(img, (0, pad_w, 0, pad_h))
+
+        return img
+
+
+# class Preprocess(nn.Module):
+#     """ Module to pre-process data coming from the dataset """
+
+#     @torch.no_grad()    # disable gradients for efficiency
+#     def forward(self, img, seg:bool, do2D:bool, binary:bool) -> torch.Tensor:
+
+#         #Normalize the MRI image
+#         if not seg:
+#             img = normalize(img)
+
+#         # Get a 2D slice if specified
+#         if self.do2D:
+#             img= get_central_slice(img)
+
+#         # Convert segmentation mask to binary classification labels (tumor vs. non-tumor)
+#         if self.binary and seg:
+#             img[img > 0] = 1
+
+#         # Convert numpy arrays to PyTorch tensors and add a channel dimension
+#         if not seg: # MRI Image
+#             img = torch.from_numpy(img).unsqueeze(0).float()
+#         else: # Segmentation mask
+#             img = torch.from_numpy(img).unsqueeze(0).long()
+
+#         return img
