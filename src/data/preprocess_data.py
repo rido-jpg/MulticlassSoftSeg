48b0e56fb3585e92a97b77ca155f886473267915
rido-jpg <razai.farid@gmail.com>
2024-06-04 15:03:59 +0200

    Changed Path

--- src/data/preprocess_data.py
+++ src/data/preprocess_data.py
@@ -4,7 +4,7 @@ from pathlib import Path
 # Preprocess Nifti images used for training, validation and testing
 # A script that is only used once, which loops over the whole BIDS folder structure, and saves all files in fnio (fast numpy io) format in the same folders with the same name (just the file ending is replaced by .fnio).
 
-bids_root_path = '/media/DATA/farid_ma/dev/multiclass_softseg/MulticlassSoftSeg/data/external/ASNR-MICCAI-BraTS2023-GLI-Challenge/ASNR-MICCAI-BraTS2023-GLI-Challenge-TrainingData'
+bids_root_path = '/home/student/farid_ma/dev/multiclass_softseg/MulticlassSoftSeg/data/external/ASNR-MICCAI-BraTS2023-GLI-Challenge/ASNR-MICCAI-BraTS2023-GLI-Challenge-TrainingData'
 list_of_path_dicts = create_bids_path_list_of_dicts(bids_root_path)
 
 save_bids_niftis_as_fnio(list_of_path_dicts)
