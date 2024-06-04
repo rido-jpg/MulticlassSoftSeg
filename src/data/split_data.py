3c9477562a80012cbd21f28c2451e6306afac62b
rido-jpg <razai.farid@gmail.com>
2024-06-04 16:10:21 +0200

    Changed hardcoded split to 70/15/15

--- src/data/split_data.py
+++ src/data/split_data.py
@@ -5,7 +5,7 @@ bids_root_path = '/home/student/farid_ma/dev/multiclass_softseg/MulticlassSoftSe
 split_target_path = '/home/student/farid_ma/dev/multiclass_softseg/MulticlassSoftSeg/data/external/ASNR-MICCAI-BraTS2023-GLI-Challenge'
 
 list_of_path_dicts = create_bids_path_list_of_dicts(bids_root_path)
-split_data_and_save_to_csv(list_of_path_dicts, [0.8, 0.1, 0.1], split_csv_save_dir, 42) 
+split_data_and_save_to_csv(list_of_path_dicts, [0.7, 0.15, 0.15], split_csv_save_dir, 42) 
 
 split_csv = '/home/student/farid_ma/dev/multiclass_softseg/MulticlassSoftSeg/src/data/train_val_test_split.csv'
 
