7db661f35a7368fa9b18da2a137d5ff657d4bb07
rido-jpg <razai.farid@gmail.com>
2024-07-11 15:01:11 +0200

    added learningratemonitor
    added transforms
    
    added parameters for model (dim, groups, grad_accumulation)and training (contrast, resizing)
    
    added automatic adjustment of logged model name if augmentations are available or not

--- src/pl_train.py
+++ src/pl_train.py
@@ -1,8 +1,10 @@
 import os
 import lightning.pytorch as pl
+from lightning.pytorch.callbacks import LearningRateMonitor
 from pl_unet import LitUNetModule
 from data.bids_dataset import BidsDataModule, brats_keys
 from lightning.pytorch.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
 from monai.transforms import (
     Compose,
     RandAdjustContrastd,
@@ -13,19 +15,24 @@ from monai.transforms import (
     SpatialPadd,
     CastToTyped,
     ToTensord,
+    RandScaleIntensityd,
+    RandShiftIntensityd,
 )
 import numpy as np
 import torch
 import math
 
+
 if __name__ == '__main__':
     # Set device automatically handled by PyTorch Lightning
     data_dir = '/home/student/farid_ma/dev/multiclass_softseg/MulticlassSoftSeg/data/external/ASNR-MICCAI-BraTS2023-GLI-Challenge'
+    #data_dir = '/home/student/farid_ma/dev/multiclass_softseg/MulticlassSoftSeg/data/external/ASNR-MICCAI-BraTS2023-GLI-Challenge/Sample-Subset'
     in_channels = 1 # MRI scans are grayscale -> 1 channel
     n_classes = 4   # we have 4 classes (background, edema, non-enhancing tumor, enhancing tumor)
     out_channels = n_classes    # as we don't have intermediate feature maps, our output are the final class predictions
     img_key = brats_keys[0]
     do2D = False     # Use slices and 2D Unet or whole MRI and 3D Unet
+    contrast = 't1c'
 
     if n_classes == 2:
         binary = True
@@ -33,55 +40,54 @@ if __name__ == '__main__':
         binary = False
 
     # Hyperparameters
-    start_lr = 0.001   # starting learning rate
+    start_lr = 0.0001   # starting learning rate
     lr_end_factor = 0.01 # factor to reduce learning rate to at the end of training
     l2_reg_w = 0.001    # weight for L2 regularization
     dsc_loss_w = 1.0    # weight for Dice loss
-    batch_size = 4     # batch size
-    max_epochs = 40    # number of epochs to train
+    batch_size = 1     # batch size
+    max_epochs = 800    # number of epochs to train
+    dim = 16    # number of filters in the first layer (has to be divisible by number of groups)
+    groups = 8  # number of groups for group normalization 
+    resize = (200, 200, 152) # resize the input images to this size
+    accumulate_grad_batches = 4
 
-    # augmentations = Compose(
-    #     [   
-    #         RandAdjustContrastd(keys=img_key, prob=0.8, gamma=(0.7,1.5)),
-    #         RandRotated(keys=brats_keys, range_x=math.radians(30), range_y=math.radians(30), range_z=math.radians(30), prob=0.3,keep_size=True, mode =["bilinear", "nearest"]),
-    #         RandAffined(keys=brats_keys, prob=0.4, translate_range=(10, 10, 10), scale_range=(0.1, 0.1, 0.1), mode =["bilinear", "nearest"]),
-    #         RandGaussianNoised(keys=img_key, prob=0.1, mean=0.0, std=0.1),
-    #         RandFlipd(keys=brats_keys, prob=0.5, spatial_axis=0),
-    #         SpatialPadd(keys=brats_keys, spatial_size=(256, 256, 256), mode="constant"),
-    #         CastToTyped(keys=brats_keys, dtype=(torch.float, torch.long)),
-    #         #ToTensord(keys=brats_keys),
-    #     ]
-    # )
-    
     augmentations = Compose(
         [   
-            RandAdjustContrastd(keys=img_key, prob=0.8, gamma=(0.7,1.5)),
-            RandRotated(keys=brats_keys, range_x=math.radians(30), range_y=math.radians(30), range_z=math.radians(30), prob=0.75,keep_size=True, mode =["bilinear", "nearest"]),
-            RandAffined(keys=brats_keys, prob=0.75, translate_range=(10, 10, 10), scale_range=(0.1, 0.1, 0.1), mode =["bilinear", "nearest"]),
-            RandGaussianNoised(keys=img_key, prob=0.1, mean=0.0, std=0.1),
+            #RandAdjustContrastd(keys=img_key, prob=0.8, gamma=(0.7,1.5)),
+            #RandRotated(keys=brats_keys, range_x=math.radians(30), range_y=math.radians(30), range_z=math.radians(30), prob=0.75,keep_size=True, mode =["bilinear", "nearest"]),
+            #RandAffined(keys=brats_keys, prob=0.75, translate_range=(10, 10, 10), scale_range=(0.1, 0.1, 0.1), mode =["bilinear", "nearest"]),
+            #RandGaussianNoised(keys=img_key, prob=0.1, mean=0.0, std=0.1),
             RandFlipd(keys=brats_keys, prob=0.5, spatial_axis=0),
-            #ToTensord(keys=brats_keys),
+            RandFlipd(keys=brats_keys, prob=0.5, spatial_axis=1),
+            RandFlipd(keys=brats_keys, prob=0.5, spatial_axis=2),
+            RandScaleIntensityd(keys=img_key, factors=0.1, prob=1.0),
+            RandShiftIntensityd(keys=img_key, offsets=0.1, prob=1.0),
         ]
     )
-
     #augmentations = None
 
     model = LitUNetModule(
-        in_channels = 1,
+        in_channels = in_channels,
         out_channels = out_channels,
+        dim = dim,
+        groups = groups,
         do2D = do2D,
+        binary= binary, 
         start_lr = start_lr,
         lr_end_factor = lr_end_factor,
         n_classes = n_classes,
         l2_reg_w = l2_reg_w,
         epochs = max_epochs,
     )  
+
     data_module = BidsDataModule(
         data_dir = data_dir,
+        contrast = contrast,
         do2D = do2D,
         binary = binary,
-        batch_size = batch_size,
+        batch_size = batch_size,‚‚
         train_transform = augmentations,
+        resize = resize,
         test_transform=None,
     )
     
@@ -90,10 +96,15 @@ if __name__ == '__main__':
     else:
         model_name='3D_UNet'
 
-    suffix = str(f"_batch_size_{batch_size}_n_epochs_{max_epochs}__binary:{binary}_with_augmentations")
+    
+    if augmentations == None:
+        suffix = str(f"_batch_size_{batch_size}_n_epochs_{max_epochs}_dimUNet_{dim}_binary:{binary}_no_augmentations")
+    else:
+        suffix = str(f"_batch_size_{batch_size}_n_epochs_{max_epochs}_dimUNet_{dim}_binary:{binary}_with_augmentations")
+
+    #suffix = suffix + "_DEBUGGING_RUN"
 
     #Directory for logs
-    #filepath_logs = os.getcwd() + "/lightning_logs/"
     filepath_logs = '/home/student/farid_ma/dev/multiclass_softseg/MulticlassSoftSeg/src/logs/lightning_logs'
 
     # Determine next version number
@@ -117,6 +128,9 @@ if __name__ == '__main__':
     #profiler = pl.profilers.AdvancedProfiler(dirpath=dirpath, filename='profiler_logs')
     profiler = pl.profilers.SimpleProfiler(dirpath=dirpath, filename='performance_logs')
 
+    # Learning rate monitor
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+
     # Trainer handles training loop
     trainer = pl.Trainer(
         #fast_dev_run=True,
@@ -125,9 +139,12 @@ if __name__ == '__main__':
         log_every_n_steps=10, 
         accelerator='auto',
         logger=logger,
+        callbacks=lr_monitor,
         #profiler=profiler,
-        profiler='simple'
+        profiler='simple',
+        accumulate_grad_batches=accumulate_grad_batches,
     )
-    
+
+
     # Train the model
     trainer.fit(model, datamodule=data_module)
