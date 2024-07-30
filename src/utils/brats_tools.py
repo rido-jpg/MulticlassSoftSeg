930949b78f961d432a21acf80219a3da2bee5e28
rido-jpg <razai.farid@gmail.com>
2024-07-30 16:56:55 +0200

    Fixed plot_slices function: title shows up at top of plot and saved image

--- src/utils/brats_tools.py
+++ src/utils/brats_tools.py
@@ -18,17 +18,17 @@ def plot_slices(mri_slice, seg_slice, plt_title:str="" , omit_background=True, s
     ax[0].set_title('MRI Slice')
     ax[1].set_title('Segmentation Slice')
 
+    if plt_title:
+        plt.suptitle(plt_title, fontsize=30)
+
     if omit_background==True:
         # Only overlay areas where mask is True, with the segmentation mask using a colormap
         ax[1].imshow(masked_seg_slice.T, cmap='jet', alpha=0.5, origin='lower')
-        
     else:
         ax[1].imshow(seg_slice.T, cmap='jet', alpha = 0.5, origin='lower')      #Overlay with transparency
 
     if save_path:
         plt.savefig(save_path)  # Save the figure to the specified path
-
-    #plt.title(plt_title)
 
     if show:
         plt.show()      # Show the plot if requested
