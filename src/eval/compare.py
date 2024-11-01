413bacb08fdc8ef2163db2c307c20f1f926aae2e
rido-jpg <razai.farid@gmail.com>
2024-11-01 10:15:09 +0100

    changed hardcoded directories of statistic files and added # %% to create jupyter executable cells inside pyhtoin script

--- src/eval/compare.py
+++ src/eval/compare.py
@@ -1,8 +1,9 @@
+# %%
 from panoptica.panoptica_statistics import Panoptica_Statistic
 
 
-hard = Panoptica_Statistic.from_file("src/eval/3D_UNet_v1__baseline_softmax.tsv")
-soft = Panoptica_Statistic.from_file("src/eval/3D_UNet_v7__MIXED_MSE.tsv")
+hard = Panoptica_Statistic.from_file("/home/student/farid_ma/dev/multiclass_softseg/MulticlassSoftSeg/src/eval/3D_UNet_v0__Hard_Baseline.tsv")
+soft = Panoptica_Statistic.from_file("/home/student/farid_ma/dev/multiclass_softseg/MulticlassSoftSeg/src/eval/3D_UNet_v1__exp_1_mixed_loss.tsv")
 
 print("\n\n\n")
 print(f"SUMMARY FOR HARD MODEL")
@@ -11,3 +12,4 @@ hard.print_summary()
 print("\n\n\n")
 print(f"SUMMARY FOR SOFT MODEL")
 soft.print_summary()
+# %%
